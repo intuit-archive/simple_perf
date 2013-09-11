8b4baf815cff9e2952a2919a1399a91fc1d4f36c
sadams1 <sean_adams@intuit.com>
2013-09-11 10:28:17 -0700

    adding ability for mulitple user sim files in gatling

--- lib/simple_perf/cli/start_gatling.rb
+++ lib/simple_perf/cli/start_gatling.rb
@@ -18,9 +18,11 @@ EOS
           opt :help, "Display Help"
           opt :environment, "Set the target environment", :type => :string
           opt :name, "Stack name to manage", :type => :string
+          opt :simulation, "Gatling User simulation file (e.g. live_community.TaxUserModelSimulation)", :type => :string
         end
         Trollop::die :environment, "is required but not specified" unless opts[:environment]
         Trollop::die :name, "is required but not specified" unless opts[:name]
+        Trollop::die :simulation, "is required but not specified" unless opts[:simulation]
 
         config = Config.new.environment opts[:environment]
 
@@ -30,7 +32,7 @@ EOS
         command = 'simple_deploy execute' +
                     ' -e ' + opts[:environment] +
                     ' -n ' + opts[:name] +
-                    ' -c "cd ~/gatling_test_files; nohup ./gatling.sh < input > gatling.log &"' +
+                    ' -c "cd ~/gatling_test_files; nohup ./gatling.sh ' + opts[:simulation] + ' < input > gatling.log  &"' +
                     ' -l debug'
 
         Shared::pretty_print `#{command}`
