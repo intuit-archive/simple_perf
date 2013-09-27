3e8b8c4781bac606de5e0d50ba3d1da7ccca52b1
Jimmy Armitage <jimmy_armitage@intuit.com>
2013-09-26 21:48:44 -0700

    In destroy combine deleting instances and bucket into one command

--- lib/simple_perf/cli/destroy.rb
+++ lib/simple_perf/cli/destroy.rb
@@ -10,7 +10,7 @@ module SimplePerf
           version SimplePerf::VERSION
           banner <<-EOS
 
-Destroys CloudFormation stack.
+Destroys CloudFormation stacks.
 
 Usage:
       simple_perf destroy -e ENVIRONMENT -p PROJECT_NAME
@@ -27,6 +27,52 @@ EOS
                         ' -n ' + 'simple-perf-' + opts[:project]
 
         Shared::pretty_print `#{command}`
+
+        config = Config.new.environment opts[:environment]
+
+        AWS.config(
+            :access_key_id => config['access_key'],
+            :secret_access_key => config['secret_key'])
+
+        command = 'simple_deploy list' +
+            ' -e ' + opts[:environment] +
+            ' | grep ' + opts[:project] + '-s3'
+        bucket_stack = `#{command}`
+
+        puts bucket_stack
+
+        if bucket_stack.empty?
+          return
+        end
+
+        command = 'simple_deploy outputs' +
+            ' -e ' + opts[:environment] +
+            ' -n ' + bucket_stack
+        bucket_name = `#{command}`
+        bucket_name = bucket_name.split(' ')[1]
+
+        if(config['region'] == 'us-east-1')
+          s3_endpoint = 's3.amazonaws.com'
+        else
+          s3_endpoint = "s3-#{config['region']}.amazonaws.com"
+        end
+
+        s3 = AWS::S3.new(:s3_endpoint => s3_endpoint)
+
+        # use existing s3 bucket
+        b = s3.buckets[bucket_name]
+
+        b.objects.each do |obj|
+          puts obj.key
+          obj.delete
+        end
+
+        command = 'simple_deploy destroy' +
+            ' -e ' + opts[:environment] +
+            ' -n ' + bucket_stack
+
+        Shared::pretty_print `#{command}`
+
       end
     end
   end
